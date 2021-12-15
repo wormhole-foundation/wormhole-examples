3cc2d5623ed1c6ed39a30b65233810159f679b74
Bruce Riley <briley@jumptrading.com>
2021-12-15 01:46:47 +0000

    Eth to Solana integration test

--- spy_relay/src/__tests__/integration.ts
+++ spy_relay/src/__tests__/integration.ts
@@ -1,13 +1,23 @@
 import {
+  approveEth,
+  attestFromEth,
   attestFromSolana,
   CHAIN_ID_ETH,
   CHAIN_ID_SOLANA,
   createWrappedOnEth,
+  createWrappedOnSolana,
+  getEmitterAddressEth,
   getEmitterAddressSolana,
+  getForeignAssetSolana,
+  // getIsTransferCompletedSolana,
   // getIsTransferCompletedEth,
   hexToUint8Array,
   nativeToHexString,
+  postVaaSolana,
+  parseSequenceFromLogEth,
   parseSequenceFromLogSolana,
+  redeemOnSolana,
+  transferFromEth,
   transferFromSolana,
   uint8ArrayToHex,
 } from "@certusone/wormhole-sdk";
@@ -26,9 +36,10 @@ import {
   Token,
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
 import axios from "axios";
 import {
+  ETH_CORE_BRIDGE_ADDRESS,
   ETH_NODE_URL,
   ETH_PRIVATE_KEY,
   ETH_PUBLIC_KEY,
@@ -38,6 +49,7 @@ import {
   SOLANA_PRIVATE_KEY,
   SOLANA_TOKEN_BRIDGE_ADDRESS,
   SPY_RELAY_URL,
+  TEST_ERC20,
   TEST_SOLANA_TOKEN,
   WORMHOLE_RPC_HOSTS,
 } from "./consts";
@@ -213,6 +225,218 @@ describe("Solana to Ethereum", () => {
       } catch (e) {
         console.error(e);
         done("An error occurred while trying to send from Solana to Ethereum");
+      }
+    })();
+  });
+
+  test("Query Spy Relay via REST", (done) => {
+    (async () => {
+      var storeKey: string =
+        CHAIN_ID_SOLANA.toString() +
+        "/" +
+        emitterAddress +
+        "/" +
+        sequence.toString();
+      try {
+        var query: string = SPY_RELAY_URL + "/query/" + storeKey;
+        console.log("Sending query to spy relay, query: [%s]", query);
+        const result = await axios.get(query);
+        console.log(
+          "status: ",
+          result.status,
+          ", statusText: ",
+          result.statusText,
+          ", data: ",
+          result.data
+        );
+
+        expect(result).toHaveProperty("status");
+        expect(result.status).toBe(200);
+        expect(result).toHaveProperty("data");
+
+        console.log(result.data);
+        done();
+      } catch (e) {
+        console.error(e);
+        done("An error occurred while trying to send query to spy relay");
+      }
+    })();
+  });
+});
+
+describe("Ethereum to Solana", () => {
+  test("Attest Ethereum ERC-20 to Solana", (done) => {
+    (async () => {
+      try {
+        // create a signer for Eth
+        const provider = new ethers.providers.WebSocketProvider(ETH_NODE_URL);
+        const signer = new ethers.Wallet(ETH_PRIVATE_KEY, provider);
+        // attest the test token
+        const receipt = await attestFromEth(
+          ETH_TOKEN_BRIDGE_ADDRESS,
+          signer,
+          TEST_ERC20
+        );
+        // get the sequence from the logs (needed to fetch the vaa)
+        const sequence = parseSequenceFromLogEth(
+          receipt,
+          ETH_CORE_BRIDGE_ADDRESS
+        );
+        const emitterAddress = getEmitterAddressEth(ETH_TOKEN_BRIDGE_ADDRESS);
+        // poll until the guardian(s) witness and sign the vaa
+        const { vaaBytes: signedVAA } = await getSignedVAAWithRetry(
+          WORMHOLE_RPC_HOSTS,
+          CHAIN_ID_ETH,
+          emitterAddress,
+          sequence,
+          {
+            transport: NodeHttpTransport(),
+          }
+        );
+        // create a keypair for Solana
+        const keypair = Keypair.fromSecretKey(SOLANA_PRIVATE_KEY);
+        const payerAddress = keypair.publicKey.toString();
+        // post vaa to Solana
+        const connection = new Connection(SOLANA_HOST, "confirmed");
+        await postVaaSolana(
+          connection,
+          async (transaction) => {
+            transaction.partialSign(keypair);
+            return transaction;
+          },
+          SOLANA_CORE_BRIDGE_ADDRESS,
+          payerAddress,
+          Buffer.from(signedVAA)
+        );
+        // create wormhole wrapped token (mint and metadata) on solana
+        const transaction = await createWrappedOnSolana(
+          connection,
+          SOLANA_CORE_BRIDGE_ADDRESS,
+          SOLANA_TOKEN_BRIDGE_ADDRESS,
+          payerAddress,
+          signedVAA
+        );
+        // sign, send, and confirm transaction
+        try {
+          transaction.partialSign(keypair);
+          const txid = await connection.sendRawTransaction(
+            transaction.serialize()
+          );
+          await connection.confirmTransaction(txid);
+        } catch (e) {
+          // this could fail because the token is already attested (in an unclean env)
+        }
+        provider.destroy();
+        done();
+      } catch (e) {
+        console.error(e);
+        done(
+          "An error occurred while trying to attest from Ethereum to Solana"
+        );
+      }
+    })();
+  });
+  // TODO: it is attested
+  test("Send Ethereum ERC-20 to Solana", (done) => {
+    (async () => {
+      try {
+        // create a keypair for Solana
+        const connection = new Connection(SOLANA_HOST, "confirmed");
+        const keypair = Keypair.fromSecretKey(SOLANA_PRIVATE_KEY);
+        const payerAddress = keypair.publicKey.toString();
+        // determine destination address - an associated token account
+        const solanaMintKey = new PublicKey(
+          (await getForeignAssetSolana(
+            connection,
+            SOLANA_TOKEN_BRIDGE_ADDRESS,
+            CHAIN_ID_ETH,
+            hexToUint8Array(nativeToHexString(TEST_ERC20, CHAIN_ID_ETH) || "")
+          )) || ""
+        );
+        const recipient = await Token.getAssociatedTokenAddress(
+          ASSOCIATED_TOKEN_PROGRAM_ID,
+          TOKEN_PROGRAM_ID,
+          solanaMintKey,
+          keypair.publicKey
+        );
+        // create the associated token account if it doesn't exist
+        const associatedAddressInfo = await connection.getAccountInfo(
+          recipient
+        );
+        if (!associatedAddressInfo) {
+          const transaction = new Transaction().add(
+            await Token.createAssociatedTokenAccountInstruction(
+              ASSOCIATED_TOKEN_PROGRAM_ID,
+              TOKEN_PROGRAM_ID,
+              solanaMintKey,
+              recipient,
+              keypair.publicKey, // owner
+              keypair.publicKey // payer
+            )
+          );
+          const { blockhash } = await connection.getRecentBlockhash();
+          transaction.recentBlockhash = blockhash;
+          transaction.feePayer = keypair.publicKey;
+          // sign, send, and confirm transaction
+          transaction.partialSign(keypair);
+          const txid = await connection.sendRawTransaction(
+            transaction.serialize()
+          );
+          await connection.confirmTransaction(txid);
+        }
+        // create a signer for Eth
+        const provider = new ethers.providers.WebSocketProvider(ETH_NODE_URL);
+        const signer = new ethers.Wallet(ETH_PRIVATE_KEY, provider);
+        const amount = parseUnits("1", 18);
+        // approve the bridge to spend tokens
+        await approveEth(ETH_TOKEN_BRIDGE_ADDRESS, TEST_ERC20, signer, amount);
+        // transfer tokens
+        const receipt = await transferFromEth(
+          ETH_TOKEN_BRIDGE_ADDRESS,
+          signer,
+          TEST_ERC20,
+          amount,
+          CHAIN_ID_SOLANA,
+          hexToUint8Array(
+            nativeToHexString(recipient.toString(), CHAIN_ID_SOLANA) || ""
+          )
+        );
+        // get the sequence from the logs (needed to fetch the vaa)
+        sequence = parseSequenceFromLogEth(receipt, ETH_CORE_BRIDGE_ADDRESS);
+        emitterAddress = getEmitterAddressEth(ETH_TOKEN_BRIDGE_ADDRESS);
+        // poll until the guardian(s) witness and sign the vaa
+        const { vaaBytes: signedVAA } = await getSignedVAAWithRetry(
+          WORMHOLE_RPC_HOSTS,
+          CHAIN_ID_ETH,
+          emitterAddress,
+          sequence,
+          {
+            transport: NodeHttpTransport(),
+          }
+        );
+        // post vaa to Solana
+        await postVaaSolana(
+          connection,
+          async (transaction) => {
+            transaction.partialSign(keypair);
+            return transaction;
+          },
+          SOLANA_CORE_BRIDGE_ADDRESS,
+          payerAddress,
+          Buffer.from(signedVAA)
+        );
+        // expect(
+        //   await getIsTransferCompletedSolana(
+        //     SOLANA_TOKEN_BRIDGE_ADDRESS,
+        //     signedVAA,
+        //     connection
+        //   )
+        // ).toBe(false);
+        provider.destroy();
+        done();
+      } catch (e) {
+        console.error(e);
+        done("An error occurred while trying to send from Ethereum to Solana");
       }
     })();
   });
