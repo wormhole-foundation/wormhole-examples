6d79694da5cc219719ec2da2c83e2d432b9c5d05
Paul Noel <panoel007@gmail.com>
2022-01-14 13:16:34 +0000

    Add default metrics

--- pyth_relay/src/promHelpers.ts
+++ pyth_relay/src/promHelpers.ts
@@ -9,6 +9,7 @@ import client = require("prom-client");
 export class PromHelper {
   private register = new client.Registry();
   private walletReg = new client.Registry();
+  private collectDefaultMetrics = client.collectDefaultMetrics;
 
   // Actual metrics
   private seqNumGauge = new client.Gauge({
@@ -61,7 +62,7 @@ export class PromHelper {
     this.register.setDefaultLabels({
       app: name,
     });
-
+    this.collectDefaultMetrics({ register: this.register });
     // Register each metric
     this.register.registerMetric(this.seqNumGauge);
     this.register.registerMetric(this.successCounter);
