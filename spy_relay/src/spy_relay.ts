dc55817d6f426cbd8e81caaebc6bdc8491233ef3
bruce-riley <96066700+bruce-riley@users.noreply.github.com>
2022-02-09 08:01:45 -0600

    Pyth relay (#14)
    * initial commit
    
    * Rework to make it pyth instead of spy
    
    * Add command to listen to mainnet to readme
    
    * More code rework
    
    * Dump productId from messages
    
    * Fixes for message parsing
    
    * Drop duplicate messages
    
    * Start of creating docker file for pyth_relay
    
    * Trying to get docker  to listen to spy_guardian
    
    * Proving that docker image works
    
    * Publish to Terra
    
    * Keep connection to Terra open all the time
    
    * Add timing to terra relayer code
    
    * Not passing fees into transaction
    
    * Use Winston logger
    
    * Update commads to build and run spy guardian
    
    * Specify config file via env variable
    
    * Console logging not honoring log level
    
    * Remove dependence on redis
    
    * Add support for listen only mode
    
    * number of publishes is off by one
    
    * Only one worker should use the wallet at a time
    
    * Add prometheus metrics
    
    * Add ability to query wallet balance
    
    * Add latency histogram
    
    * Use a condition variable rather than sleeping to reduce delays
    
    * Periodically query the wallet balance
    
    * Add wallet balance metrics
    
    * Add incoming packet counter
    
    * Enable strict type checking
    
    * Add design document
    
    * Adding more stuff
    
    * Remove use of deprecated methods.
    
    * Back out bug from "fixing" deprecated code.
    
    * Remove deprecated methods
    
    * fix: Improvements to Dockerfile Wormhole pyth_relay and spy_guardian (#10)
    
    Co-authored-by: Eran Davidovich <edavidovich@jumptrading.com>
    
    * Revert dockerfile
    
    * fix: fix Dockerfile entrypoint (#11)
    
    also modify docs to reflect the change.
    
    Co-authored-by: Eran Davidovich <edavidovich@jumptrading.com>
    
    * Batch messages into a single tx per block
    
    * Update the testnet parms in env.sample
    
    * Reduce lock contention in the worker
    
    * Adding already executed metric
    
    * Adding timeout metric
    
    * Peg already executed and timeout metrics
    
    * Handle "already executed" exception
    
    * Add wallet update time for metrics
    
    * Catch silent failure on relay
    
    * Log file not getting written if error on init
    
    * Add timestamp label to wallet metric
    
    * Add support for readiness probe
    
    * Make spy_relay support listen_only mode
    
    * Add default metrics
    
    * Manage seq num locally
    
    * Manage seq num locally
    
    * Oops! Didn't mean to commit "noEmit"!
    
    * Update docs
    
    * Delete app.js
    
    This file is not relevant to pyth_relay
    
    Co-authored-by: Paul Noel <panoel007@gmail.com>
    Co-authored-by: Eran Davidovich <erancx@users.noreply.github.com>
    Co-authored-by: Eran Davidovich <edavidovich@jumptrading.com>
    Co-authored-by: Paul Noel <35237584+panoel@users.noreply.github.com>

--- spy_relay/src/spy_relay.ts
+++ spy_relay/src/spy_relay.ts
@@ -1,80 +1,204 @@
-import { setDefaultWasm } from "@certusone/wormhole-sdk/lib/cjs/solana/wasm";
+import {
+  createSpyRPCServiceClient,
+  subscribeSignedVAA,
+} from "@certusone/wormhole-spydk";
 
-import { spy_listen } from "./spy_listen";
-import { spy_worker } from "./spy_worker";
-import { spy_rest } from "./spy_rest";
+import {
+  ChainId,
+  CHAIN_ID_SOLANA,
+  CHAIN_ID_TERRA,
+  hexToUint8Array,
+  uint8ArrayToHex,
+  parseTransferPayload,
+  getEmitterAddressEth,
+  getEmitterAddressSolana,
+  getEmitterAddressTerra,
+} from "@certusone/wormhole-sdk";
+
+import {
+  importCoreWasm,
+  setDefaultWasm,
+} from "@certusone/wormhole-sdk/lib/cjs/solana/wasm";
+
+// import { storeKeyFromParsedVAA, storePayloadFromVaaBytes } from "./helpers";
 import * as helpers from "./helpers";
-import { RelayerEnvironment, validateEnvironment } from "./configureEnv";
 
-require("dotenv").config();
+var pendingMap = new Map<string, string>();
+pendingMap.set("XXX", "XXX should be first");
+pendingMap.set("CCC", "CCC should be second");
+pendingMap.set("XXX", "XXX should still be first");
+pendingMap.set("AAA", "AAA should be third");
 
-setDefaultWasm("node");
+for (let [pk, pendingValue] of pendingMap) {
+  console.log("key: [" + pk + "], value: [" + pendingValue + "]");
+}
 
-const env: RelayerEnvironment = validateEnvironment();
+while (pendingMap.size !== 0) {
+  const first = pendingMap.entries().next();
 
-var runListen: boolean = true;
-var runWorker: boolean = true;
-var runRest: boolean = true;
-var foundOne: boolean = false;
+  console.log(
+    "deleting first item, which is: key: [" +
+      first.value[0] +
+      "], value: [" +
+      first.value[1] +
+      "]"
+  );
 
+  pendingMap.delete(first.value[0]);
+
+  for (let [pk, pendingValue] of pendingMap) {
+    console.log("key: [" + pk + "], value: [" + pendingValue + "]");
+  }
+}
+
+var listenOnly: boolean = false;
 for (let idx = 0; idx < process.argv.length; ++idx) {
   if (process.argv[idx] === "--listen_only") {
-    if (foundOne) {
-      console.error(
-        'May only specify one of "--listen_only", "--worker_only" or "--rest_only"'
-      );
-      process.exit(1);
-    }
-    runWorker = false;
-    runRest = false;
-    foundOne = true;
-  }
-
-  if (process.argv[idx] === "--worker_only") {
-    if (foundOne) {
-      console.error(
-        'May only specify one of "--listen_only", "--worker_only" or "--rest_only"'
-      );
-      process.exit(1);
-    }
-    runListen = false;
-    runRest = false;
-    foundOne = true;
-  }
-
-  if (process.argv[idx] === "--rest_only") {
-    if (foundOne) {
-      console.error(
-        'May only specify one of "--listen_only", "--worker_only" or "--rest_only"'
-      );
-      process.exit(1);
-    }
-    runListen = false;
-    runWorker = false;
-    foundOne = true;
+    console.log("running in listen only mode, will not forward to redis");
+    listenOnly = true;
+    break;
   }
 }
 
-// Start the spy listener to listen to the guardians.
-if (runListen) {
-  spy_listen();
+require("dotenv").config();
+if (!process.env.SPY_SERVICE_HOST) {
+  console.error("Missing environment variable SPY_SERVICE_HOST");
+  process.exit(1);
 }
 
-// Start the spy worker to process VAAs from the store.
-if (runWorker) {
-  spy_worker();
+// Connect to redis
+import { createClient } from "redis";
+
+async function connectToRedis() {
+  var rClient = createClient();
+
+  rClient.on("error", (err) => console.log("Redis Client Error", err));
+  rClient.on("connect", (err) => console.log("Redis Client Connected", err));
+
+  console.log("Attempting to connect...");
+
+  await rClient.connect();
+  return rClient;
 }
 
-// Start the REST server, if configured.
-if (runRest && process.env.SPY_REST_PORT) {
-  var restPort = parseInt(process.env.SPY_REST_PORT);
-  if (!restPort) {
-    console.error(
-      "Environment variable SPY_REST_PORT is set to [%s], which is not a valid port number.",
-      process.env.SPY_REST_PORT
+async function storeInRedis(
+  client: typeof myRedisClient,
+  name: string,
+  value: string
+) {
+  await (await client).set(name, value);
+}
+
+if (false) {
+  var myRedisClient;
+  if (!listenOnly) {
+    myRedisClient = connectToRedis();
+  }
+
+  console.log(
+    "spy_relay starting up, will listen for signed VAAs from [%s]",
+    process.env.SPY_SERVICE_HOST
+  );
+
+  setDefaultWasm("node");
+
+  (async () => {
+    var filter = {};
+    if (process.env.SPY_SERVICE_FILTERS) {
+      const parsedJsonFilters = eval(process.env.SPY_SERVICE_FILTERS);
+
+      var myFilters = [];
+      for (var i = 0; i < parsedJsonFilters.length; i++) {
+        var myChainId = parseInt(parsedJsonFilters[i].chain_id) as ChainId;
+        var myEmitterAddress = await encodeEmitterAddress(
+          myChainId,
+          parsedJsonFilters[i].emitter_address
+        );
+        var myEmitterFilter = {
+          emitterFilter: {
+            chainId: myChainId,
+            emitterAddress: myEmitterAddress,
+          },
+        };
+        console.log(
+          "adding filter: chainId: [%i], emitterAddress: [%s]",
+          myEmitterFilter.emitterFilter.chainId,
+          myEmitterFilter.emitterFilter.emitterAddress
+        );
+        myFilters.push(myEmitterFilter);
+      }
+
+      console.log("setting", myFilters.length, "filters");
+      filter = {
+        filters: myFilters,
+      };
+    } else {
+      console.log("processing all signed VAAs");
+    }
+
+    const client = createSpyRPCServiceClient(process.env.SPY_SERVICE_HOST);
+    const stream = await subscribeSignedVAA(client, filter);
+
+    const { parse_vaa } = await importCoreWasm();
+
+    stream.on("data", ({ vaaBytes }) => {
+      processVaa(parse_vaa, vaaBytes);
+    });
+
+    console.log("spy_relay waiting for transfer signed VAAs");
+  })();
+}
+
+async function encodeEmitterAddress(
+  myChainId,
+  emitterAddressStr
+): Promise<string> {
+  if (myChainId === CHAIN_ID_SOLANA) {
+    return await getEmitterAddressSolana(emitterAddressStr);
+  }
+
+  if (myChainId === CHAIN_ID_TERRA) {
+    return await getEmitterAddressTerra(emitterAddressStr);
+  }
+
+  return getEmitterAddressEth(emitterAddressStr);
+}
+
+function processVaa(parse_vaa, vaaBytes) {
+  // console.log(vaaBytes);
+  const parsedVAA = parse_vaa(hexToUint8Array(vaaBytes));
+  // console.log(parsedVAA);
+  if (parsedVAA.payload[0] === 1) {
+    if (!listenOnly) {
+      var storeKey = helpers.storeKeyFromParsedVAA(parsedVAA);
+      var storePayload = helpers.storePayloadFromVaaBytes(vaaBytes);
+      //    console.log("storeKey: ", helpers.storeKeyToJson(storeKey));
+      //    console.log("storePayload: ", helpers.storePayloadToJson(storePayload));
+      var newStoreKey = helpers.storeKeyFromJson(JSON.stringify(storeKey));
+      var newStorePayload = helpers.storeePayloadFromJson(
+        JSON.stringify(storePayload)
+      );
+      //    console.log("newStoreKey: ", newStoreKey);
+      //    console.log("newStorePayload: ", newStorePayload);
+      storeInRedis(
+        myRedisClient,
+        helpers.storeKeyToJson(storeKey),
+        helpers.storePayloadToJson(storePayload)
+      );
+    }
+
+    var transferPayload = parseTransferPayload(Buffer.from(parsedVAA.payload));
+    console.log(
+      "transfer: emitter: [%d:%s], seqNum: %d, payload: origin: [%d:%s], target: [%d:%s],  amount: %d",
+      parsedVAA.emitter_chain,
+      uint8ArrayToHex(parsedVAA.emitter_address),
+      parsedVAA.sequence,
+      transferPayload.originChain,
+      transferPayload.originAddress,
+      transferPayload.targetChain,
+      transferPayload.targetAddress,
+      transferPayload.amount
     );
-    process.exit(1);
+    // console.log(transferPayload);
   }
-
-  spy_rest(restPort);
 }
